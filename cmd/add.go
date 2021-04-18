b92919996bc76eeb35cdac3da28d6da92d04ff66
maruimarui0101 <22180616+maruimarui0101@users.noreply.github.com>
2021-04-18 15:37:41 +0100

    Initial commit, list and add commands added, file saving and reading capabilities added

---
+++ cmd/add.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/maruimarui0101/tri/todo"
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a new todo",
+	Long:  `Add will create a new todo item to the list`,
+	// can define function here
+	Run: addRun,
+}
+
+// better to add function separately!
+func addRun(cmd *cobra.Command, args []string) {
+	fmt.Println("add called!")
+
+	items := []todo.Item{}
+
+	for _, x := range args {
+		items = append(items, todo.Item{Text: x})
+	}
+	err := todo.SaveItems("reminders.json", items)
+
+	if err != nil {
+		fmt.Errorf("%v", err)
+	}
+
+	// fmt.Printf("%#v\n", items)
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
